09a0b91ec6d02a3553ae83d78a045de1cf433c3f
rishab1128 <rishabpurkayastha28@gmail.com>
2021-06-26 17:39:53 +0530

    upd:Typing Effect added in Welcome section

---
+++ app.js
@@ -0,0 +1,81 @@
+/*----Typing Effect-----*/
+
+//ES6 Classes
+class TypeWriter{
+    constructor(txtElement, cursorElement, words , wait=2000){
+        this.txtElement=txtElement;
+        this.cursorElement=cursorElement;
+        this.words=words;
+        this.wait=parseInt(wait,10);
+        this.txt='';
+        this.wordIndex=0;
+        this.type();
+        this.isDeleting=false;
+    }
+
+    type(){
+        if(!(this.cursorElement.classList.contains('typing'))) {
+            this.cursorElement.classList.add('typing');
+        }
+        //Current index of word
+        const current=this.wordIndex % this.words.length;
+        //Get full text of current word
+        const fullTxt=this.words[current];
+
+        //Check if deleting
+        if(this.isDeleting){
+            //Remove char
+            this.txt=fullTxt.substring(0,this.txt.length-1);
+        }
+        else{
+            //Add char
+            this.txt=fullTxt.substring(0,this.txt.length+1)
+        }
+        
+        //Insert txt into element
+        this.txtElement.innerHTML=`<span class="txt">${this.txt}</span>`
+
+        //Initial Type Speed
+        let typeSpeed=200;
+
+        if(this.isDeleting){
+            typeSpeed/=2;
+        }
+
+        //If word is complete
+        if(!this.isDeleting && this.txt===fullTxt){
+            this.cursorElement.classList.remove('typing');
+            //Make a pause at end
+            typeSpeed=this.wait;
+            //Set delete to true
+            this.isDeleting=true;
+        }
+        else if(this.isDeleting && this.txt===''){
+            this.cursorElement.classList.remove('typing');
+            this.isDeleting=false;
+            //Move to next word
+            this.wordIndex++;
+            //Pause a little bit before start typing
+            typeSpeed=500;
+        }
+        setTimeout(()=>this.type(),typeSpeed);
+    }
+
+}
+
+
+//Init on DOM Load
+document.addEventListener('DOMContentLoaded',init);
+
+//Init App
+function init(){
+    const txtElement=document.querySelector('.txt-type');
+    const words=JSON.parse(txtElement.getAttribute('data-words'));
+    const wait=txtElement.getAttribute('data-wait');
+    const cursorElement = document.querySelector('.cursor');
+
+    //Init TypeWriter
+    new TypeWriter(txtElement, cursorElement, words , wait);
+
+}
+
